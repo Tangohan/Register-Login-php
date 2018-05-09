db393f08498b4bcbf80c63c6e4a2da3f71d2dd33
Tangohan <30940812+Tangohan@users.noreply.github.com>
2018-05-09 14:03:02 +0200

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,35 @@
+<?php
+ob_start();
+session_start();
+
+//set timezone
+date_default_timezone_set('Europe/London');
+
+//database credentials
+define('DBHOST','');
+define('DBUSER','');
+define('DBPASS','');
+define('DBNAME','');
+
+
+//application address
+define('DIR','http://leagueofboosted.fr/');
+define('SITEEMAIL','league-of-boosted@gmail.com');
+
+try {
+
+	//create PDO connection
+	$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+	//show error
+    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
+    exit;
+}
+
+//include the user class, pass in the database connection
+include('classes/user.php');
+include('classes/phpmailer/mail.php');
+$user = new User($db);
+?>
